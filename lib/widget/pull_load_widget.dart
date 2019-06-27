6f908673b207382354cdc2def161031677a70b58
Rannie <18622218653@163.com>
2019-06-27 22:40:14 +0800

    add refresh control

--- lib/widget/pull_load_widget.dart
+++ lib/widget/pull_load_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rann_github/widget/flare_pull_controller.dart';
+import 'package:rann_github/widget/refresh_sliver.dart' as IOS;
 
 const double iosRefreshHeight = 140;
 const double iosRefreshIndicatorExtent = 100;
@@ -31,12 +32,25 @@ class HubPullLoadWidget extends StatefulWidget {
 class _HubPullLoadWidgetState extends State<HubPullLoadWidget>
     with HubFlarePullController {
 
+  final GlobalKey<IOS.CupertinoSliverRefreshControlState> siiverRefreshKey =
+      GlobalKey<IOS.CupertinoSliverRefreshControlState>();
+
+  ScrollController _scrollController;
+  bool isRefreshin = false;
+  bool isLoadingMorning = false;
+
   @override
   ValueNotifier<bool> isActive = ValueNotifier<bool>(true);
 
   @override
   // TODO: implement getPlayAuto
   bool get getPlayAuto => false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
